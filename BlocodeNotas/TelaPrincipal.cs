95c56c7afd98ff728a3725bbb6fca6066da95082
pedro.hsalves2 <pedro.hsalves2@SBC0505222W10-1>
2022-04-18 11:57:04 -0300

    Adicionar arquivos de projeto.

---
+++ BlocodeNotas/TelaPrincipal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlocodeNotas
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Rtf = string.Empty;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Abrir Arquivo";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Arquivos Txt | *.txt";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader leitor = new System.IO.StreamReader(openFileDialog1.FileName);
+                richTextBox1.Text = leitor.ReadToEnd();
+                leitor.Close();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Salvar Arquivo";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Arquivo txt| *.txt";
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter gravar = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                gravar.Write(richTextBox1.Text);
+                gravar.Close();
+            }
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
